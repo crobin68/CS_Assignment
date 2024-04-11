4361c41915a66f8b8312c21912c926e581fb701f
crobin68 <crobin68@msudenver.edu>
2024-04-11 13:37:52 -0600

    main menu if user puts 0 or 3 it tells them wrong input and goes back to main_menu_option

--- 2/OFFICAL_programming_assi_2.py
+++ 2/OFFICAL_programming_assi_2.py
@@ -13,17 +13,17 @@ def selection():
     print("Here are your options")
     print("1.) Add an event")
     print("2.) View upcoming events")
-    option = input("Please choose an option= ")
+    main_menu_option = input("Please choose an option= ")
 
 
 
 # main menu option #1 (Add event)
-    if int(option) == 1:
+    if int(main_menu_option) == 1:
         print("Add an event")
 
 
 # main menu option #2 (View upcoming events)
-    elif int(option) == 2:
+    elif int(main_menu_option) == 2:
         print("======================================================================\n"
         "You selected option 2, please wait for the options to load...\n"
         "======================================================================")
@@ -44,20 +44,20 @@ def selection():
             print("2.) Annual Gala - 2024-06-01")
 
             #blank text
-            print("")
+            print("3.) ***Please add an event***")
 
 
 
             pick_event = input("Please pick an event to see more detail = ")
 
             # make sure user chooses options 1-3
-            if int(pick_event) < 0 or int(pick_event) > 3:
+            if int(pick_event) < 0 or int(pick_event) > 4:
                 print("\033[1m****INVALID OPTION****\033[0m")
                 print("PLease choose an option 1-3")
                 choosing_option_for_upcomming_events()
 
 
-
+#======================================================================================================================================================================================================================================
             if int(pick_event) == 1:
 
 
@@ -77,29 +77,25 @@ def selection():
                 print("========================================")
 
                 # prompt user if they want to go to main menu
-                startover = input("Would you like to go to the beggining?\n 1) Yes\n 2) No\n please type in the option you want = ")
-                
-                
-                
+                def go_to_main_menu():
+                    startover = input("Would you like to go to the beggining?\n 1) Yes\n 2) No\n please type in the option you want = ")
+                    
                     # when in event detil #1 give user option to go to main menu or term the code
-                if int(startover) == 1:
-                    selection()
-                    # end the program if you selects (2)
-                elif int(startover) == 2:
-                    print("program ending in")
-                    time.sleep(1)
-                    print("3...")
-                    time.sleep(1)
-                    print("2...")
-                    time.sleep(1)
-                    print("1...")
-                    time.sleep(1)
-                    print("\033[31m Terminating program \033[0m")
-                    sys.exit()
-
-
-
-                
+                    if int(startover) == 1:
+                        selection()
+                        # end the program if you selects (2)
+                    elif int(startover) == 2:
+                        print("program ending in")
+                        time.sleep(1)
+                        print("3...")
+                        time.sleep(1)
+                        print("2...")
+                        time.sleep(1)
+                        print("1...")
+                        time.sleep(1)
+                        print("\033[31m Terminating program \033[0m")
+                        sys.exit()
+     
             elif int(pick_event) == 2:
 
                                        #header           #title (header)      #title (decript)    #Date: (header)   #Date (decpipt)   #Attendees: (header)   #the rest is list of people going
@@ -117,10 +113,14 @@ def selection():
                 print("========================================")
 
                 print("option 2")
+            
 
 
 
+            elif int(pick_event) == 3:
+                print("option 3")
 
+#======================================================================================================================================================================================================================================
 
 
 
@@ -143,21 +143,25 @@ def selection():
                 print("")
                 print("")
                 
-                choosing_option_for_upcomming_events() # make user go back to 
-        choosing_option_for_upcomming_events() #calling the opt 1 and 2 selection
+                choosing_option_for_upcomming_events() # make user go back to
+
+
+        choosing_option_for_upcomming_events() #calling the whole block of code
         
         
         
+  #==============================================================================
+ #          if user selects 3 in main menu tell them tou select between 1-2
 
+    elif int(main_menu_option) >= 3 or int(main_menu_option) <= 0:
+        print("********************************************************")
+        print("| Unknown option selected, please select another option |")
+        print("********************************************************")
 
-    elif int(option) == 3:
-        print("View event details")
-
-
-
-    elif int(option) >= 3:
-        print("unknown option selected, please select another option")
         selection()
+
+  #==============================================================================
+
 
 # call selection (with out this nothing would happen)
 selection()
