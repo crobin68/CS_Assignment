5a07af47224c03db8ed6afc60662df090dac4e15
crobin68 <crobin68@msudenver.edu>
2024-04-16 19:59:00 -0600

    FINISHED ALL MY CODE AFTER 40 fucking hours and i could of just spent 5 hours on it :SOB:

--- DEBUG_CODE_2.py
+++ DEBUG_CODE_2.py
@@ -1,124 +1,134 @@
 import time
-
-events = []
-# welcome message 
-def welcome_message():
-    print("Welcome to your event planner")
+import sys
 
 
 
 
+def ALL_CODE():
+    #set a empty list to (events)
+    events = []
 
 
-# this is to add the event from the users input
-def add_event():
-    def user_date():
+    # welcome message 
+    def welcome_message():
+        print("Welcome to your event planner")
+        time.sleep(2)
+
+    # this is to add the event from the users input
+    def add_event():
+        def user_date():        # I COULD OF USED THE timedate import but some how i did not know what it was until all my code was done
+            while True:
+                year = input("Please enter a Year : ")
+                if year.isdigit() and 2023 < int(year) < 9999:
+                    break
+                print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+                print("\033[1;31m Please enter a valid number between 2024 and 9999. \033[0m")
+                print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+            while True:
+                month = input("Please enter the month number: ")
+                if month.isdigit() and 1 <= int(month) <= 12:
+                    break
+                print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+                print("\033[1;31m Please enter a valid month number between 1 and 12. \033[0m")
+                print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+            while True:
+                day = input("Please enter a Day number: ")
+                if day.isdigit() and 1 <= int(day) <= 31:
+                    break
+                print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+                print("\033[1;31m Please enter a valid day number between 1 and 31. \033[0m")
+                print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+
+            return f"{year}-{month}-{day}" # making sure you have all the dates in one format
+
+        title = input("Enter an Event Title = ")    # get users input for the event title
+        
+        date = user_date()      # get (date) from (user_date())
+        #=======================================================================================================================================
+        
+        
+        
+        attendees = []  # make an empty list so the people can be imported into it
+
+        done_entered = False # set (done_entered) to a false state
+        while not done_entered:
+            attendee = input("Enter the name of the new attendee (or type 'Done' when finished): ")
+            if attendee.lower() == "done":  # check to see if input is lowcased (done)
+                done_entered = True         # stop the loop when (done_entered) is True
+                break                       # break the loop 
+            if attendee == "":      # check if the user entered a blank name for the attendee
+                print("Please enter a valid name, you left the last one blank (no worries I did not add it to the list)")
+            else:
+                attendees.append(attendee)  # Append attendees to the list
+
+
+
+        #when the user creates an event it uses this empty libery and puts all user defined vars into it
+        events.append({"Title": title, "Date": date, "Attendees": attendees}) 
+        
+        print("You have added an event to your list") # just confirming to the user that the event was added
+
+
+
+
+
+    # if there are no events this message displays 
+    def view_events():
+        if not events:
+            print("No upcoming events.") # if there are no events in the list display this message
+            return
+        print("Upcoming events:") 
+        for index, event in enumerate(events, start=1):     # if there are events that were created show the list of them
+            print(f"{index}.) {event['Title']} - {event['Date']}")
+
+
+    def view_event_details():
+        view_events()   # call the code block (view_events)
+        event_index = input("Select an event to view details (enter the event number): ") # select an event that you want to view
+        try:
+            event_index = int(event_index)
+            if 1 <= event_index <= len(events):
+                event = events[event_index - 1]
+                #prints all the event details 
+                print("========================================")
+                print("Event Details:")
+                print(f"Title: {event['Title']}")   
+                print(f"Date: {event['Date']}")
+                print("\x1b[4m:Attendees:\033[0m")                 
+                for attendee in event['Attendees']:
+                    print(f"- {attendee}")  # print all the people in a list (formated up to down)
+                print("========================================")
+            else:
+                print("Invalid event number. Please enter a valid event number.") # if the user inputs a number that is not there send user to menu
+        except ValueError:
+            print("Invalid input. Please enter a valid event number.")
+
+
+    def event_planner():
+        welcome_message() # send the welcome message 
         while True:
-            year = input("Please enter a Year : ")
-            if year.isdigit() and 2023 < int(year) < 9999:
-                break
-            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-            print("\033[1;31m Please enter a valid number between 2024 and 9999. \033[0m")
-            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+            print("\033[0m Please select an option \033[0m")
+            print("\033[1m 1) Add an event \033[0m")
+            print("\033[1m 2) View upcoming events \033[0m")
+            option = input("\033[3m Please choose an option: \033[0m")
 
-        while True:
-            month = input("Please enter the month number: ")
-            if month.isdigit() and 1 <= int(month) <= 12:
-                break
-            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-            print("\033[1;31m Please enter a valid month number between 1 and 12. \033[0m")
-            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-
-        while True:
-            day = input("Please enter a Day number: ")
-            if day.isdigit() and 1 <= int(day) <= 31:
-                break
-            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-            print("\033[1;31m Please enter a valid day number between 1 and 31. \033[0m")
-            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-
-        return f"{year}-{month}-{day}" # making sure you have all the dates in one format
-
-    title = input("Enter an Event Title = ")
-    date = user_date()
-    #=======================================================================================================================================
-    
-    
-    
-    attendees = []  # make an empty list so the people can be imported into it
-
-    done_entered = False # set (done_entered) to a false state
-    while not done_entered:
-        attendee = input("Enter the name of the new attendee (or type 'Done' when finished): ")
-        if attendee.lower() == "done":
-            done_entered = True
-        else:
-            attendees.append(attendee)  # Append attendee to the list
+            if option == '1': # if user input is ('1') then execute the block (add_event)
+                add_event()
+            elif option == '2': # if user input is ('1') then execute the block (view_event_details)
+                view_event_details()
+            elif option == '3': # if the user wants to exit the code they select option #3
+                sys.exit()
+            else:       # if anything else if typed this will prompt.
+                print("--------------------------------------------------")
+                print("\033[1m \033[1;31m Unknown option. Please select between 1 and 2 \033[0m")
+                print("--------------------------------------------------")
 
 
-    events.append({"Title": title, "Date": date, "Attendees": attendees}) # when the user creates an event it uses this empty libery
-    print("You have added an event to your list")
+    event_planner() # execute the whole code (lines 1-123) on screen
 
 
-
-
-
-# if there are no events this message displays 
-def view_events():
-    if not events:
-        print("No upcoming events.") 
-        return
-    print("Upcoming events:") 
-    for index, event in enumerate(events, start=1):
-        print(f"{index}.) {event['Title']} - {event['Date']}")
-
-
-def view_event_details():
-    view_events()   # call the code block (view_events)
-    event_index = input("Select an event to view details (enter the event number): ") # select an event that you want to view
-    try:
-        event_index = int(event_index)
-        if 1 <= event_index <= len(events):
-            event = events[event_index - 1]
-            print("========================================")
-            print("Event Details:")
-            print(f"Title: {event['Title']}")   #print user generated title
-            print(f"Date: {event['Date']}")
-            print("Attendees:")                 
-            for attendee in event['Attendees']:
-                print(f"- {attendee}")
-            print("========================================")
-        else:
-            print("Invalid event number. Please enter a valid event number.") # if the user inputs a number that is not there send user to menu
-    except ValueError:
-        print("Invalid input. Please enter a valid event number.")
-
-
-def event_planner():
-    welcome_message() # send the welcome message 
-    while True:
-        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
-        print("Please select an option")
-        print("1) Add an event")
-        print("2) View upcoming events")
-        option = input("Please choose an option: ")
-
-        if option == '1': # if user input is ('1') then execute the block (add_event)
-            add_event()
-        elif option == '2': # if user input is ('1') then execute the block (view_event_details)
-            view_event_details()
-        else:
-            print("------------------------------------------------")
-            print("Unknown option. Please select between 1 and 2")
-            print("------------------------------------------------")
-
-
-event_planner() # execute the whole code on screen
-
-
-
-
-
-
-
+ALL_CODE()
 
--- checking_for_months_with_30_days.py
+++ checking_for_months_with_30_days.py
@@ -7,6 +7,11 @@ def user_date():
         print("\033[1;31m Please enter a valid number between 2024 and 9999. \033[0m")
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
+
+
+
+
+
     while True:
         month = input("Please enter the month number: ")
         if month.isdigit() and 1 <= int(month) <= 12:
@@ -14,29 +19,71 @@ def user_date():
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
         print("\033[1;31m Please enter a valid month number between 1 and 12. \033[0m")
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
+        return month
 
-    # Define the function to get the day outside the loop
+    
+
+
+
+    """
+    get input form user 
+    IF
+    day is a number 
+    and
+    day is greater then or = to 1
+    and less then or = to 31
+    output the
+
+    
+    """
+    
     def loop_day():
         while True:
             day_input = input("Please enter a Day number: ")
-            if day_input.isdigit() and 1 <= int(day_input) <= 31:
-                return int(day_input)
+            if day_input.isdigit() and 1 <= int(day_input) <= 31:   #checking for number betwee 1-31
+                return int(day_input)       # to get the var outside the (loop_day)
             print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
             print("\033[1;31m Please enter a valid day number between 1 and 31. \033[0m")
             print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
-    # Call the function to get the day
+    # Calling the function to get (day) inside the loop
     day = loop_day()
 
-    # Check if the month has 30 days
-    if int(month) in [2, 4, 6, 9, 11] and day > 30:
+
+
+
+
+    """
+
+    IF the (month) input for user is (2 , 4 , 6 , 9 , 11) 
+    AND
+    If the (day) input is Greater then 30
+    THEN
+    Tell user that month has only 30 days
+
+    """
+
+    if int(month) in [2, 4, 6, 9, 11] and day > 30: #months 2,4,6,9,11 only have 30 days in the month
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
         print("\033[1;31m This month has only 30 days. Please enter a valid day. \033[0m")
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
         day = loop_day()  # Call the function again to get a valid day
 
     date = f"{year}-{month}-{day}"  # Format the date
-    return date
+    
+    return date, int(month)
 
-date = user_date()
-print(date)
+
+date, month = user_date()
+
+
+def word_month():
+    word_month = ["January","Febuary","March","April","May","June","July","Augest","September","October","November","December"]
+
+    list_month = str([word_month[month-1]])[2:-2]
+    print(f"the month you selected is {list_month}")
+    return list_month
+
+
+
+print(f"The date you entered {word_month()} {date}")
